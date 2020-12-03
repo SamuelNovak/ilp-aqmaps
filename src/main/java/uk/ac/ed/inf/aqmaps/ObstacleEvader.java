82e00b04a6fd278f4052b8495f27c9c1a2266627
Samuel Novak <samuel.novak@protonmail.com>
2020-12-03 02:49:29 +0000

    If you can't avoid all zones, at least cross all of them

--- src/main/java/uk/ac/ed/inf/aqmaps/ObstacleEvader.java
+++ src/main/java/uk/ac/ed/inf/aqmaps/ObstacleEvader.java
@@ -12,16 +12,18 @@ import com.mapbox.geojson.Polygon;
 
 public class ObstacleEvader {
 
-	private FeatureCollection noFlyZones;
+	private ArrayList<ArrayList<Point>> noFlyZones;
 	
 	// this will hold points close to the points of noFlyZones, but outside the zones
+	// indices coupled with noFlyZones: element i for zoneExpansions corresponds to element i of noFlyZones
 	private ArrayList<ArrayList<Point>> zoneExpansions;
 	
-	public ObstacleEvader(FeatureCollection noFlyZones) {
-		this.noFlyZones = noFlyZones;
-		
+	public ObstacleEvader(FeatureCollection noFlyZones) {		
 		var zones = noFlyZones.features();
+		
+		this.noFlyZones = new ArrayList<ArrayList<Point>>();
 		zoneExpansions = new ArrayList<ArrayList<Point>>();
+		
 		for (int i = 0; i < zones.size(); i++) {
 			var polygon = (Polygon) zones.get(i).geometry();
 			// get external coordinates
@@ -32,48 +34,66 @@ public class ObstacleEvader {
 				offsetPoints.add(createOffsetPoint(pt, points));
 			}
 			
+			// TODO zoneExpansions simplification - if two points are closer that SENSOR_DISTANCE, contract them
+			// alternatively - make sure all points are at least SENSOR_DIST away from the polygon
 			zoneExpansions.add(offsetPoints);
 		}
 	}
 	
-	public ArrayList<Polygon> crossedObstacles(Point a1, Point a2) {
-		var ret = new ArrayList<Polygon>();
-		for (var ftr : noFlyZones.features()) {
-			assert(ftr.geometry() instanceof Polygon); // TODO nejak lepsie toto urobit
-			var polygon = (Polygon) ftr.geometry();
-			
-			if (crossesObstacle(a1, a2, polygon))
-				ret.add(polygon);
+	public ArrayList<ArrayList<Point>> crossedObstacles(Point a1, Point a2) {
+		ArrayList<ArrayList<Point>> ret = new ArrayList<ArrayList<Point>>();
+		for (var zone : noFlyZones) {			
+			if (crossesObstacle(a1, a2, zone))
+				ret.add(zone);
 		}
 		return ret;
 	}
 	
-	private boolean crossesObstacle(Point a1, Point a2, Polygon obstacle) {
-		// Getting the 0th element = the external outline (don't care if they have interior holes)
-		var points = obstacle.coordinates().get(0);
-		
-		// Iterate until the second-to-last, the last one needs to wrap around (path is cyclic) and so will be handled separately
-		for (int i = 0; i < points.size() - 1; i++) {
-			if (linesIntersect(a1, a2, points.get(i), points.get(0)))
-				// Found an intersection with a no-fly zone
-				return true;
-		}
-		// Here handle the final line segment
-		if (linesIntersect(a1, a2, points.get(0), points.get(points.size() -1)))
-			// Found an intersection
-			return true;
-		
-		return false;
+	private boolean crossesObstacle(Point a1, Point a2, ArrayList<Point> points) {		
+		return !obstacleIntersections(a1, a2, points).isEmpty();
 	}
 	
+	/** Find if and where a line intersects an obstacle (no fly zone).
+	 * @param a1 Line start point
+	 * @param a2 Line end point
+	 * @param points Obstacle
+	 * @return list of pairs: left: intersection point;
+	 * 						  right: corresponding index i, such that intersection occurred with line points[i] -- points[i+1] (mod points.size())
+	 */
+	private ArrayList<Pair<Point, Integer>> obstacleIntersections(Point a1, Point a2, ArrayList<Point> points) {
+		var intersections = new ArrayList<Pair<Point, Integer>>();
+		// Iterate until the second-to-last, the last one needs to wrap around (path is cyclic) and so will be handled separately
+		for (int i = 0; i < points.size() - 1; i++) {
+			var intsection = intersection(a1, a2, points.get(i), points.get((i+1) % points.size()));
+			if (intsection != null)
+				// Found an intersection with a no-fly zone
+				intersections.add(new Pair<Point, Integer>(intsection, i));
+		}
+		// Here handle the final line segment
+		var intsection = intersection(a1, a2, points.get(0), points.get(points.size() - 1));
+		if (intsection != null)
+			// Found an intersection
+			intersections.add(new Pair<Point, Integer>(intsection, points.size() - 1));
+		
+		return intersections;
+	}
+	
+	/* TODO dead code
 	private boolean linesIntersect(Point a1, Point a2, Point b1, Point b2) {
 		Line2D line_a = new Line2D.Double(a1.latitude(), a1.longitude(), a2.latitude(), a2.longitude());
 		Line2D line_b = new Line2D.Double(b1.latitude(), b1.longitude(), b2.latitude(), b2.longitude());
 		return line_a.intersectsLine(line_b);		
 	}
+	*/
 	
 	/** Computes the point of intersection of the two lines
+	 * @param a1 Start point of first line
+	 * @param a2 End point of first line
+	 * @param b1 Start point of second line
+	 * @param b2 End point of second line
+	 * @return
 	 */
+	// TODO move up
 	private Point intersection(Point a1, Point a2, Point b1, Point b2) {
 		var a1x = a1.longitude();
 		var a1y = a1.latitude();
@@ -111,27 +131,27 @@ public class ObstacleEvader {
 	}
 
 	/** Compute waypoints to get from origin_point to next_point and avoid no fly zones.
-	 * @param origin_point
-	 * @param next_point
+	 * @param originPoint
+	 * @param nextPoint
 	 * @return Sequence of waypoints
 	 */
-	public ArrayList<Point> waypointsToAvoidAllObstacles(Point origin_point, Point next_point) {
+	public ArrayList<Point> waypointsToAvoidAllObstacles(Point originPoint, Point nextPoint) {
 		// find first obstacle that stands in the way
 		// avoid it
 		// call self with this avoided obstacle in clearedObstacles
 		// recursive run won't care about the clearedObstacles anymore - speedup
 		
 		var waypoints = new ArrayList<Point>();
-		var lastPoint = origin_point;
+		var lastPoint = originPoint;
 		
-		var nearestIntersectingObstacle = findNearestIntersectingObstacle(origin_point, next_point);
+		var nearestIntersectingObstacle = findNearestIntersectingObstacle(originPoint, nextPoint);
 		while (nearestIntersectingObstacle != null) {
 			Point nearestIntersection = nearestIntersectingObstacle.left;
-			Polygon nearestPolygon = nearestIntersectingObstacle.right;
+			ArrayList<Point> nearestPolygon = nearestIntersectingObstacle.right;
 			
 			// This assumes that avoiding obstacles separately and putting the waypoints together will not create a longer path
 			// as if we avoided all obstacles at once. That might not be the case, but it is a good heuristic.
-			waypoints.addAll(waypointsToAvoidSingleObstacle(lastPoint, next_point, nearestPolygon, RotationDirection.Both));
+			waypoints.addAll(waypointsToAvoidSingleObstacle(lastPoint, nextPoint, nearestPolygon));
 			
 			// TODO debug
 			
@@ -148,7 +168,7 @@ public class ObstacleEvader {
 			
 			if (!waypoints.isEmpty())
 				lastPoint = waypoints.get(waypoints.size() - 1);
-			nearestIntersectingObstacle = findNearestIntersectingObstacle(lastPoint, next_point);
+			nearestIntersectingObstacle = findNearestIntersectingObstacle(lastPoint, nextPoint);
 		}
 		
 		return waypoints;
@@ -160,80 +180,73 @@ public class ObstacleEvader {
 	 * @param obstacle
 	 * @param preferredDirection
 	 * 		  0 go both ways, +1 positive (counterclockwise), -1 negative (clockwise)
-	 * @return List of waypoints, or null if unreachable using these specific arguments.
+	 * @return List of waypoints, or null if unreachable for some reason (should not happen if valid and reasonable no-fly-zones are supplied)
 	 */
-	private ArrayList<Point> waypointsToAvoidSingleObstacle(Point origin_point, Point next_point, Polygon obstacle, RotationDirection direction) {
-		// base case - no more waypoints needed
-		if (!crossesObstacle(origin_point, next_point, obstacle))
+	private ArrayList<Point> waypointsToAvoidSingleObstacle(Point origin_point, Point next_point, ArrayList<Point> obstaclePoints) {
+		// find out which obstacle this is
+		var obstacleIndex = noFlyZones.indexOf(obstaclePoints);
+		// get the corresponding expanded zone
+		var expandedZone = zoneExpansions.get(obstacleIndex);
+		// find intersections with this expanded zone
+		var intersections = obstacleIntersections(origin_point, next_point, expandedZone);
+		
+		if (intersections.isEmpty())
 			return new ArrayList<Point>();
 		
-		// get points of the obstacle polygon (index 0, want exterior)
-		var points = obstacle.coordinates().get(0);
+		// find which intersection is the nearest - this is where avoiding starts
+		double minDistance = PathPlanner.distance(origin_point, intersections.get(0).left);
+		// this will hold the index of the line that has the nearest intersection
+		int nearestIntersectedLineIndex = intersections.get(0).right;
 		
-		// some initial point (which has to exist) so we can initialize the angles
-		var pt0 = points.get(0);
-		
-		// angles from origin_point
-		var theta_min = angleBetweenPoints(origin_point, pt0);
-		var theta_min_pt = pt0;
-		
-		var theta_max = theta_min;
-		var theta_max_pt = pt0;
-		
-		// find the actual min and max theta (assuming here it's a valid polygon - has at least 3 points)
-		for (int i = 1; i < points.size(); i++) {
-			var pt = points.get(i);
-			var theta = angleBetweenPoints(origin_point, pt);
-			
-			if (theta > theta_max) {
-				theta_max = theta;
-				theta_max_pt = pt;
-			}
-			if (theta < theta_min) { // TODO together? they should not coincide in a valid polygon
-				theta_min = theta;
-				theta_min_pt = pt;
+		for (var intsection : intersections) {
+			var dist = PathPlanner.distance(origin_point, intsection.left);
+			if (dist < minDistance) {
+				minDistance = dist;
+				nearestIntersectedLineIndex = intsection.right;
 			}
 		}
 		
-		// round up or down to allowed angles to get better avoidance
-		theta_min = 10 * Math.floor(theta_min / 10);
-		theta_max = 10 * Math.ceil(theta_max / 10);
+		// now that we know on which line segment the intersection happened, we can iterate (both ways) over the points of the expanded zone,
+		// in order to find suitable waypoints that will allow the drone to avoid the obstacle
 		
-		// now generate waypoints in both ways around the polygon
-		// first waypoints are the points already found, just need to offset them so they no longer lie on the polygon
-		Point waypointThetaMin = createOffsetPoint(theta_min_pt, (ArrayList<Point>) points);
-		Point waypointThetaMax = createOffsetPoint(theta_max_pt, (ArrayList<Point>) points);
+		// doing both positive and negative in one go
+		var waypointsPositive = new ArrayList<Point>();
+		var waypointsNegative = new ArrayList<Point>();
 		
-
-		ArrayList<Point> waypointsNegative = null;
-		ArrayList<Point> waypointsPositive = null;
+		var positiveDone = false;
+		var negativeDone = false;
 		
-		if (!pointOutOfBounds(waypointThetaMin) && direction.allowsPositive()) {
-			waypointsNegative = new ArrayList<Point>();
-			waypointsNegative.add(waypointThetaMin);
+		for (int i = 0; i < expandedZone.size(); i++) {
+			var pointPositive = expandedZone.get((nearestIntersectedLineIndex + i + 1) % expandedZone.size());
+			var pointNegative = expandedZone.get((expandedZone.size() + nearestIntersectedLineIndex - i) % expandedZone.size());
 			
-			// TODO decide based on position w.r.t. obstacle - pos/neg will depend on this
-			
-			var followingWaypoints = waypointsToAvoidSingleObstacle(waypointThetaMin, next_point, obstacle, RotationDirection.Positive);
-			if (followingWaypoints == null) {
-				// impossible to go this way at some point
-				waypointsNegative = null;
-			} else {
-				waypointsNegative.addAll(followingWaypoints);
-			}
-		}
-		
-		if (!pointOutOfBounds(waypointThetaMax) && direction.allowsNegative()) {
-			waypointsPositive = new ArrayList<Point>();
-			waypointsPositive.add(waypointThetaMax);
-			
-			var followingWaypoints = waypointsToAvoidSingleObstacle(waypointThetaMax, next_point, obstacle, RotationDirection.Negative);
-			if (followingWaypoints == null) {
-				// impossible to go this way at some point
+			// check within bounds
+			if (!positiveDone && pointOutOfBounds(pointPositive)) {
+				// impossible to go in the positive direction
 				waypointsPositive = null;
-			} else {
-				waypointsPositive.addAll(followingWaypoints);
+				positiveDone = true;
 			}
+			if (!negativeDone && pointOutOfBounds(pointNegative)) {
+				// impossible to go in the negative direction
+				waypointsNegative = null;
+				negativeDone = true;
+			}
+			
+			// now add the points (if necessary)
+			if (!positiveDone) {
+				waypointsPositive.add(pointPositive);
+				if (!crossesObstacle(pointPositive, next_point, obstaclePoints))
+					// this is the final waypoint, because the path is clear afterwards (in terms of the current obstacle)
+					positiveDone = true;
+			}
+			if (!negativeDone) {
+				waypointsNegative.add(pointNegative);
+				if (!crossesObstacle(pointNegative, next_point, obstaclePoints));
+			}
+			
+			// no need to iterate anymore after
+			if (positiveDone && negativeDone)
+				break;
 		}
 		
 		if (waypointsNegative == null)
@@ -241,10 +254,17 @@ public class ObstacleEvader {
 		else if (waypointsPositive == null)
 			return waypointsNegative;
 		else {
-			// pick the shorter one
-			var lengthNegative = pathLengthWaypoints(origin_point, waypointsNegative, next_point);
 			var lengthPositive = pathLengthWaypoints(origin_point, waypointsPositive, next_point);
-			if (lengthNegative < lengthPositive)
+			var lengthNegative = pathLengthWaypoints(origin_point, waypointsNegative, next_point);
+			
+			if (lengthPositive == lengthNegative) {
+				// probably won't happen, but if it does, pick the simpler one
+				System.out.println("Equal waypoint path length :O");
+				if (waypointsNegative.size() < waypointsPositive.size())
+					return waypointsNegative;
+				else
+					return waypointsPositive;
+			} else if (lengthNegative < lengthPositive)
 				return waypointsNegative;
 			else
 				return waypointsPositive;
@@ -256,22 +276,16 @@ public class ObstacleEvader {
 	 * @param next_point
 	 * @return Pair of intersection Point and its Polygon
 	 */
-	private Pair<Point, Polygon> findNearestIntersectingObstacle(Point origin_point, Point next_point) {
+	private Pair<Point, ArrayList<Point>> findNearestIntersectingObstacle(Point origin_point, Point next_point) {
 		Point nearestIntersection = null;
-		Polygon nearestPolygon = null;
+		ArrayList<Point> nearestPolygon = null;
 		
 		// -1 used because no distance will ever be < 0
 		double min_distance = -1;
 		
-		int intersections = 0;
-		
-		for (var obstacleFeature : noFlyZones.features()) {
-			// guaranteed that no fly zones will be polygons
-			var obstaclePolygon = (Polygon) obstacleFeature.geometry();
-			
-			// get (outer - hence the index 0) points of the polygon
-			var points = obstaclePolygon.coordinates().get(0);
+		for (var points : noFlyZones) {
 			final var numberOfPoints = points.size();
+			
 			for (var i = 0; i < numberOfPoints; i++) {
 				var p1 = points.get(i);
 				var p2 = points.get((i+1) % numberOfPoints);
@@ -281,21 +295,18 @@ public class ObstacleEvader {
 				if (inter != null) {
 					// intersection was found
 					var dist = PathPlanner.distance(origin_point, inter);
-					intersections++;
 					if (dist < min_distance || min_distance == -1) {
 						// this is the new minimum distance
 						nearestIntersection = inter;
-						nearestPolygon = obstaclePolygon;
+						nearestPolygon = points;
 						min_distance = dist;
 					}
 				}
 			}
 		}
 		
-		System.out.println(String.format("Intersections: %d", intersections));
-		
 		if (min_distance > -1)
-			return new Pair<Point, Polygon>(nearestIntersection, nearestPolygon);
+			return new Pair<Point, ArrayList<Point>>(nearestIntersection, nearestPolygon);
 		else
 			// no intersecting obstacle found
 			return null;
