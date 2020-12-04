e7784f6976ae617bc651e64b9c19e9d24aaa85b4
Samuel Novak <samuel.novak@protonmail.com>
2020-12-04 03:48:31 +0000

    End of Evader vol. 2

--- src/main/java/uk/ac/ed/inf/aqmaps/ObstacleEvader.java
+++ src/main/java/uk/ac/ed/inf/aqmaps/ObstacleEvader.java
@@ -1,9 +1,6 @@
 package uk.ac.ed.inf.aqmaps;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.geojson.Polygon;
@@ -13,60 +10,33 @@ public class ObstacleEvader {
 
 	private ArrayList<ArrayList<Point>> noFlyZones;
 	
-	// this will hold points close to the points of noFlyZones, but outside the zones
-	// indices coupled with noFlyZones: element i for zoneExpansions corresponds to element i of noFlyZones
-	private ArrayList<ArrayList<Point>> zoneExpansions;
-	
 	public ObstacleEvader(FeatureCollection noFlyZones) {		
 		var zones = noFlyZones.features();
 		
 		this.noFlyZones = new ArrayList<ArrayList<Point>>();
-		zoneExpansions = new ArrayList<ArrayList<Point>>();
 		
 		for (int i = 0; i < zones.size(); i++) {
 			var polygon = (Polygon) zones.get(i).geometry();
 			// get external coordinates
 			var points = polygon.coordinates().get(0);
 			this.noFlyZones.add((ArrayList<Point>) points);
-			
-			var offsetPoints = new ArrayList<Point>();
-			for (var pt : points) {
-				offsetPoints.add(createOffsetPoint(pt, points));
-			}
-			
-			// TODO zoneExpansions simplification - if two points are closer that SENSOR_DISTANCE, contract them
-			// alternatively - make sure all points are at least SENSOR_DIST away from the polygon
-			zoneExpansions.add(offsetPoints);
 		}
-		
-		
-		// TODO debug
-		var ftrs = new ArrayList<Feature>();
-		for (var points : zoneExpansions) {
-			var f = Feature.fromGeometry(Polygon.fromLngLats(List.of(points)));
-			ftrs.add(f);
-		}
-		System.out.println(FeatureCollection.fromFeatures(ftrs).toJson());
-	}
-	
-	public ArrayList<ArrayList<Point>> getNoFlyZones() {
-		return noFlyZones;
 	}
 	
 	public boolean crossesAnyObstacles(Point a1, Point a2) {
-		return !crossedObstacles(a1, a2).isEmpty();
+		return !allCrossedObstacles(a1, a2).isEmpty();
 	}
 	
-	public ArrayList<ArrayList<Point>> crossedObstacles(Point a1, Point a2) {
+	private ArrayList<ArrayList<Point>> allCrossedObstacles(Point a1, Point a2) {
 		ArrayList<ArrayList<Point>> ret = new ArrayList<ArrayList<Point>>();
 		for (var zone : noFlyZones) {			
-			if (crossesObstacle(a1, a2, zone))
+			if (crossesOneObstacle(a1, a2, zone))
 				ret.add(zone);
 		}
 		return ret;
 	}
 	
-	private boolean crossesObstacle(Point a1, Point a2, ArrayList<Point> points) {		
+	private boolean crossesOneObstacle(Point a1, Point a2, ArrayList<Point> points) {		
 		return !obstacleIntersections(a1, a2, points).isEmpty();
 	}
 	
@@ -94,14 +64,6 @@ public class ObstacleEvader {
 		
 		return intersections;
 	}
-	
-	/* TODO dead code
-	private boolean linesIntersect(Point a1, Point a2, Point b1, Point b2) {
-		Line2D line_a = new Line2D.Double(a1.latitude(), a1.longitude(), a2.latitude(), a2.longitude());
-		Line2D line_b = new Line2D.Double(b1.latitude(), b1.longitude(), b2.latitude(), b2.longitude());
-		return line_a.intersectsLine(line_b);		
-	}
-	*/
 	
 	/** Computes the point of intersection of the two lines
 	 * @param a1 Start point of first line
@@ -140,110 +102,6 @@ public class ObstacleEvader {
 		var x = b1x + beta*(-b1x + b2x);
 		var y = b1y + beta*(-b1y + b2y);
 		return Point.fromLngLat(x, y);
-	}
-	
-	public double evasionDistance(Point a1, Point a2) {
-		var waypoints = waypointsToAvoidAllObstacles(a1, a2);
-		return pathLengthWaypoints(a1, waypoints, a2);
-	}
-
-	/** Compute waypoints to get from origin_point to next_point and avoid no fly zones.
-	 * @param originPoint
-	 * @param nextPoint
-	 * @return Sequence of waypoints
-	 */
-	public ArrayList<Point> waypointsToAvoidAllObstacles(Point originPoint, Point nextPoint) {
-		// find first obstacle that stands in the way
-		// avoid it
-		// call self with this avoided obstacle in clearedObstacles
-		// recursive run won't care about the clearedObstacles anymore - speedup
-		
-		var waypoints = new ArrayList<Point>();
-		return waypoints;
-		/* TODO disconnected evader
-		var lastPoint = originPoint;
-		
-		var nearestIntersectingObstacle = findNearestIntersectingObstacle(originPoint, nextPoint);
-		while (nearestIntersectingObstacle != null) {
-			Point nearestIntersection = nearestIntersectingObstacle.left;
-			ArrayList<Point> nearestPolygon = nearestIntersectingObstacle.right;
-			
-			// This assumes that avoiding obstacles separately and putting the waypoints together will not create a longer path
-			// as if we avoided all obstacles at once. That might not be the case, but it is a good heuristic.
-			waypoints.addAll(waypointsToAvoidSingleObstacle(lastPoint, nextPoint, nearestPolygon));
-			
-			// TODO debug
-			
-			var ftrs = new ArrayList<Feature>();
-			for (var pt : waypoints) {
-				var f = Feature.fromGeometry(pt);
-				f.addStringProperty("marker-symbol", "marker");
-				ftrs.add(f);
-			}
-			System.out.println("Waypoints:");
-			System.out.println(FeatureCollection.fromFeatures(ftrs).toJson());
-			
-			// TODO end debug
-			
-			if (!waypoints.isEmpty())
-				lastPoint = waypoints.get(waypoints.size() - 1);
-			nearestIntersectingObstacle = findNearestIntersectingObstacle(lastPoint, nextPoint);
-		}
-		
-		return waypoints;
-		*/
-	}
-	
-	private double pathLength(ArrayList<Point> path) {
-		double length = 0;
-		for (int i = 0; i < path.size() - 1; i++)
-			length += PathPlanner.distance(path.get(i), path.get(i+1));
-		return length;
-	}
-	
-	private double pathLengthWaypoints(Point start, ArrayList<Point> waypoints, Point end) {
-		var auxList = new ArrayList<Point>();
-		auxList.add(start);
-		auxList.addAll(waypoints);
-		auxList.add(end);
-		return pathLength(auxList);
-	}
-	
-	private double angleBetweenPoints(Point a, Point b) {
-		return Math.toDegrees(Math.atan2(b.latitude() - a.latitude(), b.longitude() - a.longitude()));
-	}
-	
-	private Point createPointAtAngleDistanceFromPoint(Point origin, double length, double angle) {
-		var longitude = origin.longitude();
-		var latitude = origin.latitude();
-		
-		var angleRad = Math.toRadians(angle);
-		System.out.println(angleRad);
-		
-		var rot_longitude = longitude + length * Math.cos(angleRad);
-		var rot_latitude = latitude + length * Math.sin(angleRad);
-		return Point.fromLngLat(rot_longitude, rot_latitude);
-	}
-	
-	private Point createOffsetPoint(Point pt, List<Point> points) {
-		// TODO this function needs some love, because it's breaking everything else and my heart
-		// get the index of this point and find the previous and next point on the polygon
-		var pointIndex = points.indexOf(pt);
-		var previous = points.get((points.size() + pointIndex - 1) % points.size());
-		var next = points.get((pointIndex + 1) % points.size());
-		
-		var phiPrevious = angleBetweenPoints(pt, previous);
-		if (phiPrevious < 0)
-			phiPrevious += 360;
-		var phiNext = angleBetweenPoints(pt, next);
-		if (phiNext < 0)
-			phiNext += 360;
-		
-		var phi = ((phiPrevious + phiNext) / 2);
-		
-		System.out.println(String.format("phiPrevious = %f; priNext = %f; phi = %f", phiPrevious, phiNext, phi));
-		
-		return createPointAtAngleDistanceFromPoint(pt, 0.01 * DroneController.SENSOR_READ_MAX_DISTANCE, phi);
 	}
 
 }
